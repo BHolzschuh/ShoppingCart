9338b498690789c959034bed9d02d9463b5fa888
Brian Holzschuh <bholzschuh95@gmail.com>
2018-11-16 16:09:58 -0500

    implemented serializable users

--- src/app.java
+++ src/app.java
@@ -1,15 +1,30 @@
-import cop4331.gui.*;
+import cop4331.client.*;
 
-import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class app {
     public static void main(String[] args){
-        LoginView login = new LoginView();
-
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(login.getPanel());
-        frame.pack();
-        frame.setVisible(true);
+        Shopper brian = new Shopper("brian", "holzschuh");
+        Vendor ben = new Vendor("ben", "shapiro");
+        User[] users = new User[]{brian, ben};
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("test.dat"));
+            out.writeObject(users);
+            out.close();
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("test.dat"));
+            User[] readUsers = (User[]) in.readObject();
+            in.close();
+            for(User user: readUsers){
+                System.out.println(user.getUserName());
+                System.out.println(user.getPassword());
+                System.out.println(" ");
+            }
+        }
+        catch (Exception e){
+            System.out.println("Problem with the file");
+        }
     }
 }
--- src/cop4331/client/User.java
+++ src/cop4331/client/User.java
@@ -1,10 +1,20 @@
 package cop4331.client;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 
     public User(String userName, String password){
         this.userName = userName;
         this.password = password;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
     }
 
     private String userName;
