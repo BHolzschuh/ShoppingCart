dcf8e4e81eedd4d25c1d07ee45a07c5e90c3ebd6
Brian Holzschuh <bholzschuh95@gmail.com>
2018-11-29 15:50:11 -0500

    changed name of bundle in tester

--- src/BundleTest.java
+++ src/BundleTest.java
@@ -5,11 +5,11 @@ public class BundleTest {
     public static void main(String[] args){
         Product hammer = new Product("Hammer", 6.55, "A mighty fine hammer");
         Product nails = new Product("50 Nails", 3.00, "50 nails for hammering");
-        Bundle handn = new Bundle();
-        handn.add(hammer);
-        handn.add(nails);
-        System.out.println(handn.getName());
-        System.out.println(handn.getPrice());
-        System.out.println(handn.toString());
+        Bundle bundle = new Bundle();
+        bundle.add(hammer);
+        bundle.add(nails);
+        System.out.println(bundle.getName());
+        System.out.println(bundle.getPrice());
+        System.out.println(bundle.toString());
     }
 }
