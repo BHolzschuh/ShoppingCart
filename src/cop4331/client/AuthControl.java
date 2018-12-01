678c574a134141cb3a205177745f3e0b61558fac
BRShapiro <btqshapiro@gmail.com>
2018-11-30 19:25:03 -0500

    got rid of unndeede variables

--- src/cop4331/client/AuthControl.java
+++ src/cop4331/client/AuthControl.java
@@ -4,12 +4,8 @@ import cop4331.gui.Screen;
 
 public class AuthControl {
 
-    private final Screen screen;
-    private final AuthModel auth;
 
     public AuthControl(Screen sc, AuthModel au){
-        screen = sc;
-        auth = au;
 
         //control the view
 
@@ -44,21 +40,16 @@ public class AuthControl {
                 System.out.println(sc.getLoginUserForm().getText() + " " + sc.getLoginPwdForm().getText())
         );
 
-<<<<<<< HEAD
         sc.getRegisterSubmitButton().addActionListener(e ->
         {
             if(sc.getRegisterPwdForm().getText().equals(sc.getRegisterPwdConfirmForm().getText()))
             au.registerShopper(sc.getRegisterUserForm().getText(), sc.getRegisterPwdForm().getText());
 
         });
-||||||| merged common ancestors
-        sc.getLoginButton().addActionListener(e -> au.registerShopper(sc.getLoginPwdForm().getText(),sc.getLoginPwdForm().getText()));
-=======
         sc.getLoginButton().addActionListener(e -> au.registerShopper(sc.getLoginPwdForm().getText(),sc.getLoginPwdForm().getText()));
 
         // Back Button
         sc.getBackButton().addActionListener(e -> sc.showLogin());
->>>>>>> 54e5260c136975b75d40f06de6e1ae19881ab152
     }
 
 
