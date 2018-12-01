51b3589ac6bae8ab8a51a4727f7316584fdb6024
Brian Holzschuh <bholzschuh95@gmail.com>
2018-11-30 19:17:54 -0500

    cleaned up the app file

--- src/app.java
+++ src/app.java
@@ -1,14 +1,6 @@
 import cop4331.client.*;
-import cop4331.gui.LoginView;
-import cop4331.gui.RegisterView;
 import cop4331.gui.Screen;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.Hashtable;
-import java.util.Scanner;
 
 public class app {
     public static void main(String[] args) {
@@ -16,23 +8,6 @@ public class app {
         Screen sc = new Screen();
         AuthModel au = new AuthModel();
         AuthControl start = new AuthControl(sc,au);
-       //  au.getUsers();
-
-        /*
-        Shopper brian = new Shopper("brian", "holzschuh");
-        Vendor ben = new Vendor("ben", "shapiro");
-        Hashtable<String, User> users = new Hashtable();
-        users.put(brian.getUserName(), brian);
-        users.put(ben.getUserName(), ben);
-        */
-
-
-        /*
-        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("test.dat"));
-        out.writeObject(users);
-        out.close();
-        */
-
 
     }
 }
