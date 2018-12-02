ecd7124829c64eb5ab84aab2b43ab84db34348d3
BRShapiro <btqshapiro@gmail.com>
2018-12-02 17:34:48 -0500

    button fun

--- src/cop4331/client/Shopper.java
+++ src/cop4331/client/Shopper.java
@@ -1,6 +1,8 @@
 package cop4331.client;
 
 public class Shopper extends User{
+
+
     public Shopper(String userName, String password){
         super(userName, password);
         cart = new Cart();
